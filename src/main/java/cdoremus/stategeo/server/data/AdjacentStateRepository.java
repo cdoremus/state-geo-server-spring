3960ed24f7f23770d6e85e7f1379240439f2737d
Craig Doremus <cdoremus@gmail.com>
2015-09-11 17:58:57 -0400

    Added findAllByOrderByName method to order the data by state name

--- src/main/java/cdoremus/stategeo/server/data/AdjacentStateRepository.java
+++ src/main/java/cdoremus/stategeo/server/data/AdjacentStateRepository.java
@@ -1,7 +1,10 @@
 package cdoremus.stategeo.server.data;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AdjacentStateRepository extends MongoRepository<AdjacentState, String> {
 
+	public List<AdjacentState> findAllByOrderByName();
 }
--- src/main/java/cdoremus/stategeo/server/service/StateGeoService.java
+++ src/main/java/cdoremus/stategeo/server/service/StateGeoService.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cdoremus.stategeo.server.data.AdjacentState;
 import cdoremus.stategeo.server.data.AdjacentStateRepository;
+import cdoremus.stategeo.server.data.State;
+import cdoremus.stategeo.server.data.StateRepository;
 import cdoremus.stategeo.server.data.User;
 import cdoremus.stategeo.server.data.UserQuiz;
 import cdoremus.stategeo.server.data.UserQuizRepository;
@@ -22,6 +24,7 @@ public class StateGeoService {
 
 	private static final String USER_PATH = "/user";
 	private static final String ADJACENT_STATES_PATH = "/adjacentStates";
+	private static final String STATES_PATH = "/states";
 	private static final String USER_QUIZ_PATH = "/userQuiz";
 	
 	@Inject
@@ -29,17 +32,27 @@ public class StateGeoService {
 	@Inject
 	private AdjacentStateRepository adjacentStateRepository; 
 	@Inject
+	private StateRepository stateRepository; 
+	@Inject
 	private UserQuizRepository userQuizRepository;
 	
 
 	@RequestMapping(value=ADJACENT_STATES_PATH, method=RequestMethod.GET)
 	public List<AdjacentState> findAdjacentStates() {
 		
-		List<AdjacentState> states = adjacentStateRepository.findAll();		
+		List<AdjacentState> states = adjacentStateRepository.findAllByOrderByName();		
 		
 		return states;
 	}
 	
+	@RequestMapping(value=STATES_PATH, method=RequestMethod.GET)
+	public List<State> findAllStates() {
+		
+		List<State> states = stateRepository.findAllByOrderByName();		
+		
+		return states;
+	}
+
 	@RequestMapping(value=USER_PATH + "/{username}", method=RequestMethod.POST)
 	public User findUserByUsername(@PathVariable String username) {
 		
