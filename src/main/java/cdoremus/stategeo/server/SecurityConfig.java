e50393753c4a8f092785c3d3fb378e97e8074319
Craig Doremus <cdoremus@gmail.com>
2016-03-12 19:17:50 -0500

    Added for security configuration

---
+++ src/main/java/cdoremus/stategeo/server/SecurityConfig.java
@@ -0,0 +1,9 @@
+package cdoremus.stategeo.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
